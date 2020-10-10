81d07c29f244f1d5f2117936a049b13dfea0f236
Kuba <jciesmail@gmail.com>
2020-10-10 20:31:48 +0200

    add files

---
+++ header-secondary.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+
+
+<?php wp_head();?>
+
+
+</head>
+<body>
+
+SEcondary header
---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+
+
+<?php wp_head();?>
+
+
+</head>
+<body>
+
